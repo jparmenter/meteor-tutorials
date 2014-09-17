8a8b473c92b58023ff16d7246f2f9101642e4eb4
Jeremy Parmenter <shabado9889@gmail.com>
2014-09-16 18:46:07 -0700

    meteor tutorials init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Meteor Tutorials
+
+A collection of different meteor tutorials I've been looking at while trying out the framework.
+
+## Perquisites
+
+Meteor
+
+## Run
+
+Go to an example dir and startup the app with
+
+```
+meteor
+```
---
+++ leaderboard/leaderboard.js
@@ -0,0 +1,60 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+
+if (Meteor.isClient) {
+  Template.leaderboard.players = function () {
+    if (Session.get("selected_sort") == "name") {
+      return Players.find({}, {sort: { name: -1, score: 1}});
+    }
+    else {
+      return Players.find({}, {sort: { score: -1, name: 1}});
+    }
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    },
+    'click input.del': function() {
+      Players.remove(Session.get("selected_player"));
+    },
+    'click input.sort': function () {
+      var selected_sort = Session.get("selected_sort") == "score" ? "name" : "score";
+      Session.set("selected_sort", selected_sort);
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  });
+}
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace",
+                   "Grace Hopper",
+                   "Marie Curie",
+                   "Carl Friedrich Gauss",
+                   "Nikola Tesla",
+                   "Claude Shannon"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    }
+  });
+}
